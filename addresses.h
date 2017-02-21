f3efe5e275dd58c925d633580cd4274ead9be140
none06 <dorian.chapoulie@gmail.com>
2017-02-21 15:06:57 +0100

    Create addresses.h

---
+++ addresses.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+int oLocalPlayer = 0xAA66D4;
+int LocalPlayer;
+
+int oEntityList = 0x4AC9154;
+int ents;
+
+int oHealth = 0xFC;
+int enemyHealth;
+
+int oTeam = 0xF0;
+int enemyTeam;
+int MyTeam;
+
+int oCrosshair = 0x0000AA70;
+int cross;
+
+int oEntityLoopDistance = 0x00000010;
+
+int oAttaque = 0x2F0911C;
+int playerInCross;
+
+int toggle = 0;
+string state = "[Off]";
